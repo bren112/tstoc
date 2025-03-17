9a64c815cb57bbbba790bb720954042f6aa8b466
bren112 <brenomarinho1610@gmail.com>
2025-03-17 13:21:37 -0300

    subir primeira versao

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,44 +1,70 @@
-import React, {useState} from 'react';
-import './navbar.css'
-import { Link } from 'react-router-dom';
-import logo from './logo.png'
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom"; // Importa useLocation
+import "./navbar.css";
+import logo from "./logo.png";
+
 function Navbar() {
-    const [active , setActive] = useState("nav__menu");
-    const [toggleIcon , setToggleIcon] = useState("nav__toggler"); 
+  const [active, setActive] = useState("nav__menu");
+  const [toggleIcon, setToggleIcon] = useState("nav__toggler");
 
-    const navToggle = () =>{
-        active === 'nav__menu' ? setActive('nav__menu nav__active') : setActive('nav__menu');
+  const location = useLocation(); // Obtém a rota atual
 
-        toggleIcon === 'nav_toggler' ? 
-        setToggleIcon('nav__toggler toggle')
-        : setToggleIcon("nav__toggler")
-    }
-
+  const navToggle = () => {
+    setActive(active === "nav__menu" ? "nav__menu nav__active" : "nav__menu");
+    setToggleIcon(
+      toggleIcon === "nav_toggler" ? "nav__toggler toggle" : "nav__toggler"
+    );
+  };
 
   return (
-    <nav className='nav'>
+    <nav className="nav">
       <div className="logo">
-    {/* <Link to="/" className="nav__brand" id='logo'>FESTFY</Link> */}
-    <img id='logo' src={logo} alt="" srcset="" />
+        <img id="logo" src={logo} alt="Logo" />
       </div>
-    <ul id='links' className={active}>
-        <li className="nav__item"><Link to="/" className="nav__link">Home</Link></li>
-        <li className="nav__item"><Link to="/criar_despesa" className="nav__link">Nova Despesa</Link></li>
-        <li className="nav__item"><Link to="/solicitar_ordem" className="nav__link">Solicitar OC</Link></li>
-        <li className="nav__item"><Link to="/lista_ordem" className="nav__link">OC Solicitadas</Link></li>
-        <li className="nav__item"><Link to="/anual" className="nav__link">Anual</Link></li>
-        <li className="nav__item"><Link to="/anualcreate" className="nav__link">Criar Anual</Link></li>
- 
 
+      {/* Esconde os links se estiver na rota /home */}
+      {location.pathname !== "/" && (
+        <ul id="links" className={active}>
+          <li className="nav__item">
+            <Link to="/" className="nav__link">
+              Home
+            </Link>
+          </li>
+          <li className="nav__item">
+            <Link to="/criar_despesa" className="nav__link">
+              Nova Despesa
+            </Link>
+          </li>
+          <li className="nav__item">
+            <Link to="/solicitar_ordem" className="nav__link">
+              Solicitar OC
+            </Link>
+          </li>
+          <li className="nav__item">
+            <Link to="/lista_ordem" className="nav__link">
+              OC Solicitadas
+            </Link>
+          </li>
+          <li className="nav__item">
+            <Link to="/anual" className="nav__link">
+              Anual
+            </Link>
+          </li>
+          <li className="nav__item">
+            <Link to="/anualcreate" className="nav__link">
+              Criar Anual
+            </Link>
+          </li>
+        </ul>
+      )}
 
-    </ul>
-    <div onClick={navToggle} className={toggleIcon}>
+      <div onClick={navToggle} className={toggleIcon}>
         <div className="line1"></div>
         <div className="line2"></div>
         <div className="line3"></div>
-    </div>
-</nav>
-  )
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
